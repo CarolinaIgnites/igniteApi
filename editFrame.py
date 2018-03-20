8e71ae2307a92aa9fb5eb47dab6676f93445c054
dylan <contact@postmodern.technology>
2018-03-19 22:41:43 -0700

    cleanup

--- editFrame.py
+++ editFrame.py
@@ -1,46 +1,31 @@
-from flask import Flask, abort, make_response, redirect, request, url_for, render_template
-import base64, hashlib
+from flask import Flask, request, jsonify
+from base64 import base64encode
+from hashlib import md5
 
 import redis
+
 
 app = Flask(__name__)
 redis_connection = redis.StrictRedis("localhost", 6379)
 
-def minihash(url):
-    return base64.b64encode(hashlib.md5(url.encode(encoding='UTF-8',errors='strict')).digest())[:6]
+
+def hash(url):
+  hashed = b64encode(md5(url.encode(encoding='UTF-8',errors='strict')).digest())
+  return hashed[:6].replace('/', '_')
 
 
-@app.route('/')
-def index():
-	return render_template('index.html', iframe = 'frame.html')
-
-@app.route('/frame.html')
-def frame():
-	return render_template('frame.html')
-
-
-@app.route('/create', methods=['POST'])
+@app.route('/', methods=['POST'])
 def create_short_url():
-	long_hash = request.form['hash']
-	short_hash = minihash(long_hash)
-	short_hash.replace('/', '_')
-	redis_connection.set(short_hash, long_hash, nx=True)
-	print(short_hash)
-	#return short_hash
-	return render_template("index.html", short_hash = short_hash)
+  data = request.form['data']
+  lookup = hash(data)
+  redis_connection.set(lookup, data, nx=True)
+  return jsonify({'lookup':lookup})
 
 
-@app.route('/<string:short>')
-def get_long_url(short):
-	long_hash = redis_connection.get(short)
-
-	if (long_hash != None):
-		long_url = '/#' + long_hash
-		return redirect(long_url)
-	else:
-	 #if the short code doesn't exist go home
-		return redirect('/')
-
+@app.route('/<string:lookup>')
+def get_long_url(lookup):
+  data = redis_connection.get(lookup)
+  return jsonify({'valid': data is not None, 'data': data})
 
 if __name__ == "__main__":
     app.debug = True
