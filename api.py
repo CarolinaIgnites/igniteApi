3236f89b444ed92d743a4e950167687fbe2df481
Khalid Salah <khalidsalah@Khalids-MacBook-Pro.local>
2018-09-14 15:16:52 -0400

    testing cors proxy route, added route for displaying image in template for testing

--- api.py
+++ api.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from base64 import b64encode
 from hashlib import md5
 import redis
+import requests
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 redis_connection = redis.StrictRedis("localhost", 6379)
 
 
@@ -27,6 +28,12 @@ def create_short_url():
 def get_long_url(lookup):
   data = redis_connection.get(lookup)
   return jsonify({'valid': data is not None, 'data': data})
+
+@app.route('/khalid/<path:imgurl>')
+def get_khalid(imgurl):
+    headers = {'Access-Control-Allow-Origin' : '*'}
+    r = requests.get(imgurl, headers = headers)
+    return render_template('test.html', user_image = r.url)
 
 
 app.run()
