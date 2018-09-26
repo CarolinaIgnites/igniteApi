3030a3925706360d8de2918e81ed5874568e0226
kmsalah <khalidsalah@Khalids-MacBook-Pro.local>
2018-09-26 13:21:02 -0400

    took those git lines out

--- api.py
+++ api.py
@@ -1,22 +1,15 @@
 #!/usr/bin/python
 
-<<<<<<< HEAD
 from flask import Flask, request, jsonify, Response, stream_with_context
-=======
-from flask import Flask, request, jsonify
->>>>>>> b1b545a7c5f340942674a79a6831b6991783741f
 from base64 import b64encode
 from hashlib import md5
 import redis
 import requests
 
-<<<<<<< HEAD
-=======
 from flask import Response
 from flask import stream_with_context
 
 
->>>>>>> b1b545a7c5f340942674a79a6831b6991783741f
 app = Flask(__name__)
 redis_connection = redis.StrictRedis("localhost", 6379)
 
@@ -41,18 +34,9 @@ def get_long_url(lookup):
 
 @app.route('/cors/<path:url>')
 def proxy(url):
-<<<<<<< HEAD
     req = requests.get(url)
     response = Response(stream_with_context(req.iter_content()), content_type = req.headers['content-type'])
-    response.headers['Access-Control-Allow-Origin'] = '*'
+   # response.headers['Access-Control-Allow-Origin'] = '*'
     print(response.headers)
     return response
-=======
-    headers = {'Access-Control-Allow-Origin' : '*'}
-    resp = requests.get(url, headers = headers)
-    print(resp.headers)
-    return Response(stream_with_context(resp.iter_content()), content_type = resp.headers['content-type'])
->>>>>>> b1b545a7c5f340942674a79a6831b6991783741f
-
-
 app.run()
